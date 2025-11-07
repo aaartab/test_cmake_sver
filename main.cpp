a1a1a82789a86fd0100799228e433533b77bf3d0
zm <dev1@insider.sale>
2025-11-07 11:35:50 +0500

    added versioning

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "version.h"
 
 int main()
 {
     std::cout << "Hello, From Version Test Program!" << std::endl;
+    std::cout << "Application version: " << PROJECT_VERSION << std::endl;
 
 
     return 0;
